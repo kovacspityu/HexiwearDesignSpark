235a4f89ebad4e75dd47c39e63798bdb6897a651
mbozdal <mehmet-bozdal@hotmail.com>
2017-01-19 23:39:37 +0200

    Update serial.c

--- serial.c
+++ serial.c
@@ -12,12 +12,12 @@ int main()
     led_green=1;
     led_blue=1;
     while (true) {
-        pc.printf("The RGB led is Red\r\n"); // print the value of variable i
+        pc.printf("The RGB led is Red\r\n"); // print the situation of RGB LED
         led_red=0;
-        Thread::wait(1000); // wait a small period of time
-        pc.printf("The RGB led is off\r\n"); // print the value of variable i
+        Thread::wait(1000); // wait a second
+        pc.printf("The RGB led is off\r\n"); // print the situation of RGB LED
         led_red=1;
-        Thread::wait(1000); // wait a small period of time
+        Thread::wait(1000); // wait a second
  
     }
 }
